7b6709cc38f7c71bd0a19e7b86a165d01cccb088
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-02 20:17:55 +0100

    day 2

---
+++ src/day2.jl
@@ -0,0 +1,48 @@
+
+@inline function is_safe(it)
+    inc = true
+    dec = true
+
+    last, rest = Iterators.peel(it)
+
+    for x in rest
+        small = -(reverse(minmax(x, last))...) <= 3
+
+        inc &= x > last && small
+        dec &= x < last && small
+
+        last = x
+    end
+
+    inc || dec
+end
+
+function part1()
+    c = 0
+
+    v = Int[]
+
+    for l in eachline("$(homedir())/aoc-input/2024/day2/input")
+        empty!(v)
+        append!(v, parse(UInt8, n) for n in eachsplit(l))
+        c += is_safe(v)
+    end
+
+    c
+end
+
+function part2()
+    c = 0
+
+    v = Int[]
+
+    for l in eachline("$(homedir())/aoc-input/2024/day2/input")
+        empty!(v)
+        append!(v, parse(UInt8, n) for n in eachsplit(l))
+        c += is_safe(v) ||
+             any(is_safe((x for (i, x) in enumerate(v) if i != j))
+                 for j in eachindex(v))
+    end
+
+    c
+end
