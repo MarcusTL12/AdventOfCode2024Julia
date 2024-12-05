85536829f00333fe3e623be4ff76cad752d70101
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-05 06:13:05 +0100

    Day 5

---
+++ src/day5.jl
@@ -0,0 +1,65 @@
+
+function part1()
+    inp = read("$(homedir())/aoc-input/2024/day5/input", String)
+
+    s1, s2 = split(inp, "\n\n")
+
+    orderings = Set{NTuple{2,Int}}()
+
+    for l in eachsplit(s1, '\n')
+        a, b = eachsplit(l, '|')
+        push!(orderings, (parse(Int, a), parse(Int, b)))
+    end
+
+    s = 0
+
+    for l in eachsplit(s2, '\n')
+        if isempty(l)
+            continue
+        end
+
+        v = parse.(Int, split(l, ','))
+
+        if !all((v[i], v[i+1]) ∈ orderings for i in 1:length(v)-1)
+            continue
+        end
+
+        s += v[length(v)÷2+1]
+    end
+
+    s
+end
+
+function part1()
+    inp = read("$(homedir())/aoc-input/2024/day5/input", String)
+
+    s1, s2 = split(inp, "\n\n")
+
+    orderings = Set{NTuple{2,Int}}()
+
+    for l in eachsplit(s1, '\n')
+        a, b = eachsplit(l, '|')
+        push!(orderings, (parse(Int, a), parse(Int, b)))
+    end
+
+    s = 0
+
+    for l in eachsplit(s2, '\n')
+        if isempty(l)
+            continue
+        end
+
+        v = parse.(Int, split(l, ','))
+
+        if all((v[i], v[i+1]) ∈ orderings for i in 1:length(v)-1)
+            continue
+        end
+
+        sort!(v; lt=(x, y) -> (x, y) ∈ orderings)
+
+        s += v[length(v)÷2+1]
+    end
+
+    s
+end
+
