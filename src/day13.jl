d846139420a5218fd96e9d4d0685d09ab98bef31
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-13 11:31:20 +0100

    Rational solution

--- src/day13.jl
+++ src/day13.jl
@@ -9,23 +9,20 @@ Prize: X=(\d+), Y=(\d+)"""
     s = 0
 
     for m in eachmatch(reg, inp)
-        A = reshape(parse.(Int, m.captures[1:4]), 2, 2)
-        b = parse.(Int, m.captures[5:6])
+        A = Rational.(reshape(parse.(Int, m.captures[1:4]), 2, 2))
+        b = Rational.(parse.(Int, m.captures[5:6]))
 
         x1, x2 = A \ b
 
-        x1_int = round(Int, x1)
-        x2_int = round(Int, x2)
-
-        if abs(x1 - x1_int) < 1e-10 && abs(x2 - x2_int) < 1e-10
-            s += 3x1_int + x2_int
+        if isinteger(x1) && isinteger(x2)
+            s += 3Int(x1) + Int(x2)
         end
     end
 
     s
 end
 
-function part2()
+function part1()
     inp = read("$(homedir())/aoc-input/2024/day13/input", String)
 
     reg = r"""Button A: X\+(\d+), Y\+(\d+)
@@ -35,16 +32,13 @@ Prize: X=(\d+), Y=(\d+)"""
     s = 0
 
     for m in eachmatch(reg, inp)
-        A = reshape(parse.(BigInt, m.captures[1:4]), 2, 2)
-        b = parse.(BigInt, m.captures[5:6]) .+ 10000000000000
+        A = Rational.(reshape(parse.(Int, m.captures[1:4]), 2, 2))
+        b = Rational.(parse.(Int, m.captures[5:6])) .+ 10000000000000
 
         x1, x2 = A \ b
 
-        x1_int = round(Int, x1)
-        x2_int = round(Int, x2)
-
-        if abs(x1 - x1_int) < 1e-10 && abs(x2 - x2_int) < 1e-10
-            s += 3x1_int + x2_int
+        if isinteger(x1) && isinteger(x2)
+            s += 3Int(x1) + Int(x2)
         end
     end
 
