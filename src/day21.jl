14878282fff4334a897b4c07bdb88f2923af3e19
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-21 07:15:17 +0100

    Brute force uses too much memory and time

--- src/day21.jl
+++ src/day21.jl
@@ -139,6 +139,105 @@ function part1()
     s
 end
 
-function part2()
+function bfs2(seq, n_dir)
+    keypad = [
+        7 8 9
+        4 5 6
+        1 2 3
+        -1 0 10
+    ]
 
+    dirpad = [
+        -1 1 10
+        2 3 4
+    ]
+
+    r1 = (4, 3)
+    dir_robots = [(1, 3) for _ in 1:n_dir]
+
+    queue = [(r1, dir_robots, 1, 0)]
+    seen = Set([(r1, dir_robots, 1)])
+
+    dirs = (
+        (-1, 0),
+        (0, -1),
+        (1, 0),
+        (0, 1),
+    )
+
+    while !isempty(queue)
+        r1, dir_robots, curcode, l = popfirst!(queue)
+
+        function rec(i, key)
+            if i == 0
+                if 1 <= key <= 4
+                    d = dirs[key]
+                    npos = r1 .+ d
+                    if get(keypad, npos, -1) != -1
+                        k = (npos, dir_robots, curcode)
+                        if k ∉ seen
+                            push!(seen, k)
+                            push!(queue, (k..., l + 1))
+                        end
+                    end
+                elseif key == 10
+                    key = get(keypad, r1, -1)
+                    if key == seq[curcode]
+                        if curcode == length(seq)
+                            return l + 1
+                        else
+                            k = (r1, dir_robots, curcode + 1)
+                            if k ∉ seen
+                                push!(seen, k)
+                                push!(queue, (k..., l + 1))
+                            end
+                        end
+                    end
+                end
+                nothing
+            elseif 1 <= key <= 4
+                d = dirs[key]
+                npos = dir_robots[i] .+ d
+                if get(dirpad, npos, -1) != -1
+                    local_robots = copy(dir_robots)
+                    local_robots[i] = npos
+                    k = (r1, local_robots, curcode)
+                    if k ∉ seen
+                        push!(seen, k)
+                        push!(queue, (k..., l + 1))
+                    end
+                end
+                nothing
+            elseif key == 10
+                rec(i - 1, get(dirpad, dir_robots[i], -1))
+            end
+        end
+
+        for code in (1, 2, 3, 4, 10)
+            x = rec(length(dir_robots), code)
+            if !isnothing(x)
+                return x
+            end
+        end
+    end
+end
+
+function part2()
+    s = 0
+    for l in eachline("$(homedir())/aoc-input/2024/day21/input")
+        n = parse(Int, l[1:end-1])
+
+        seq = map(collect(l)) do c
+            if c == 'A'
+                10
+            else
+                c - '0'
+            end
+        end
+
+        len = bfs2(seq, 6)
+
+        s += n * len
+    end
+    s
 end
