2b426e5339e6c0bcc304970cff6a85f417ec7a9b
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-23 08:58:02 +0100

    using bron kerbosh

--- src/day23.jl
+++ src/day23.jl
@@ -64,34 +64,61 @@ function part2()
         end
     end
 
-    groups = Set{Vector{String}}()
-    queue = [[c] for c in computers]
+    # groups = Set{Vector{String}}()
+    # queue = [[c] for c in computers]
 
-    while !isempty(queue)
-        cur_group = popfirst!(queue)
+    # while !isempty(queue)
+    #     cur_group = popfirst!(queue)
 
-        for c in cur_group
-            for a in connections[c]
-                if a ∉ cur_group && cur_group ⊆ connections[a]
-                    new_group = [cur_group; a]
-                    sort!(new_group)
+    #     for c in cur_group
+    #         for a in connections[c]
+    #             if a ∉ cur_group && cur_group ⊆ connections[a]
+    #                 new_group = [cur_group; a]
+    #                 sort!(new_group)
 
-                    if new_group ∉ groups
-                        push!(groups, new_group)
-                        push!(queue, new_group)
-                    end
-                end
-            end
+    #                 if new_group ∉ groups
+    #                     push!(groups, new_group)
+    #                     push!(queue, new_group)
+    #                 end
+    #             end
+    #         end
+    #     end
+    # end
+
+    # max_group = String[]
+
+    # for g in groups
+    #     if length(g) > length(max_group)
+    #         max_group = g
+    #     end
+    # end
+
+    # join(max_group, ",")
+
+    join(sort!(collect(bron_kerbosh(connections,
+            Set{String}(), computers, Set{String}()))), ',')
+end
+
+function bron_kerbosh(connections, R, P, X)
+    if isempty(P) && isempty(X)
+        return R
+    end
+
+    max_clique = Set{eltype(R)}()
+
+    for v in P
+        sv = Set([v])
+
+        c = bron_kerbosh(connections,
+            R ∪ sv, P ∩ connections[v], X ∩ connections[v])
+
+        P = setdiff(P, sv)
+        X = X ∪ sv
+
+        if length(c) > length(max_clique)
+            max_clique = c
         end
     end
 
-    max_group = String[]
-
-    for g in groups
-        if length(g) > length(max_group)
-            max_group = g
-        end
-    end
-
-    join(max_group, ",")
+    max_clique
 end
